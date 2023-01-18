4a1cc230d4866f5b79e0f7ea2705bfbf083a8edf
Acrobaticbag3 <kevintom214@gmail.com>
2023-01-18 09:56:47 +0100

    Add project files.

---
+++ Uppgift 1.1/Program.cs
@@ -0,0 +1,22 @@
+﻿        //Bestämmer vilket bibliotek som ska användas.
+using System;
+
+        // Namnet på kodobjektet.
+namespace uppgift;
+
+        // Bestämmer klassen. 
+class Program
+{
+    static void Main(string[]args)
+    {  
+        // Skriver ut texten och gör en ny rad. 
+        Console.WriteLine("Du är nästan klar med kapitel 1.");
+        
+        // Skriver ut texten, inklusive " och \ och gör en ny rad. 
+        Console.WriteLine("Du har lärt dig skriva ut tecken som \" och \\");
+
+
+
+
+    }
+}
